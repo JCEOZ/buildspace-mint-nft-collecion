7aff1c43d72ad4bfb25e3c0cf193d5c3547c977a
J'CEO'Z <jaroslawzagloba@gmail.com>
2022-10-08 17:32:35 +0200

    updated test script to check if NFT is minted correctly

--- backend/scripts/run.js
+++ backend/scripts/run.js
@@ -3,6 +3,16 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to: ", nftContract.address);
+
+    // Call the contract function
+    let txn = await nftContract.makeAnEpicNFT();
+    // Wait for it to be mined
+    await txn.wait();
+
+    // mint another NFT
+    txn = await nftContract.makeAnEpicNFT();
+    // Wait for it to be mined
+    await txn.wait();
 };
 
 const runMain = async () => {
