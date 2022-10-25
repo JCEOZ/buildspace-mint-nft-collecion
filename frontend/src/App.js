a129d4e9342258b92c034d674a2ecc459b064550
J'CEO'Z <jaroslawzagloba@gmail.com>
2022-10-25 19:24:34 +0200

    checking if we are logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -10,7 +10,10 @@ const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
 
-  const checkIfWalletIsConnected = () => {
+  // Just a state variable we use to store our user's public wallet
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
 
     if (!ethereum) {
@@ -18,6 +21,18 @@ const App = () => {
       return;
     } else {
       console.log("We have the ethereum object", ethereum);
+    }
+
+    // Check if we're authorized to access the user's wallet
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    // User can have multiple authorized accounts, we grab the first one if its there!
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found!");
     }
   }
 
