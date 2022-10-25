b2e25fdb1fa60f9345494391fc7b9a6a3f1bef40
J'CEO'Z <jaroslawzagloba@gmail.com>
2022-10-25 20:59:48 +0200

    updated ABI. Added contract event listener

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
+const CONTRACT_ADDRESS = "0x20Fd93a01500a5C9ab5BA2604e8838041E5053da";
+
 const App = () => {
 
   // Just a state variable we use to store our user's public wallet
@@ -33,6 +35,7 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account: ", account);
       setCurrentAccount(account);
+      setupEventListener();
     } else {
       console.log("No authorized account found!");
     }
@@ -50,14 +53,37 @@ const App = () => {
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
       console.log("Connected: ", accounts[0]);
       setCurrentAccount(accounts[0]);
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error);
     }
   }
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x7a334D8B3E27b4Fe00C38Bb01A3533C421ADB6bE";
-
+    
     try {
       const { ethereum } = window;
 
